ca45f89550007a9273687e8f5987ccc18663911d
Dan Schultz <schultz.t.dan@gmail.com>
2015-03-15 17:39:52 -0700

    Add operations for add, subtract, multiply, divide

--- src/main.js
+++ src/main.js
@@ -1,60 +1,126 @@
-var state = 0;
-
-var displayElement = document.querySelector("#display");
-
 function main() {
+  var displayElement = document.querySelector("#display");
+
   // Append the inputted number to the current state
   var numberElements = document.querySelectorAll(".calculator--buttons--number");
   for (var i = 0; i < numberElements.length; i++) {
     numberElements[i].addEventListener("click", function(event) {
       var value = asNumber(event.target.textContent);
-      update(append(value));
+      update(displayElement, number(value));
     });
   }
 
-  var plusButton = document.querySelector("#plus");
-  plusButton.addEventListener("click", function(event) {
-    update()
+  var addButton = document.querySelector("#add");
+  addButton.addEventListener("click", function(event) {
+    update(displayElement, add());
+  });
+
+  var subtractButton = document.querySelector("#subtract");
+  subtractButton.addEventListener("click", function(event) {
+    update(displayElement, subtract());
+  });
+
+  var multiplyButton = document.querySelector("#multiply");
+  multiplyButton.addEventListener("click", function(event) {
+    update(displayElement, multiply());
+  });
+
+  var divideButton = document.querySelector("#divide");
+  divideButton.addEventListener("click", function(event) {
+    update(displayElement, divide());
   });
 
   var equalsButton = document.querySelector("#equals");
+  equalsButton.addEventListener("click", function(event) {
+    update(displayElement, equals());
+  });
 }
 
-function update(action) {
+// Model
+
+var state = {
+  operation: null,
+  value: 0,
+  replaceNext: true
+};
+
+function update(displayElement, action) {
   state = action(state);
-  displayElement.textContent = state.toString();
+  displayElement.textContent = state.value.toString();
 }
 
-// actions
+// Actions
 
-function append(value) {
+function number(value) {
   return function(state) {
-    return Number(state.toString() + value.toString());
+    return {
+      value: state.replaceNext ? value : Number(state.value.toString() + value.toString()), 
+      operation: state.operation,
+      replaceNext: false
+    };
   }
 }
 
-function add(value) {
+function equals() {
   return function(state) {
-    return state + value;
-  });
+    return {
+      value: evaluate(state),
+      operation: null,
+      replaceNext: true
+    }
+  }
 }
 
-function subtract(a, b) {
+function add() {
   return function(state) {
-    return state - value;
-  });
+    return {
+      value: state.value,
+      operation: function(value) {
+        return evaluate(state) + value;
+      },
+      replaceNext: true
+    }
+  }
 }
 
-function multiply(a, b) {
+function subtract(left) {
   return function(state) {
-    return state * value;
-  });
+    return {
+      value: state.value,
+      operation: function(value) {
+        return evaluate(state) - value;
+      },
+      replaceNext: true
+    }
+  }
 }
 
-function divide(a, b) {
+function multiply(left) {
   return function(state) {
-    return state / value;
-  });
+    return {
+      value: state.value,
+      operation: function(value) {
+        return evaluate(state) * value;
+      },
+      replaceNext: true
+    }
+  }
+}
+
+function divide(left) {
+  return function(state) {
+    return {
+      value: state.value,
+      operation: function(value) {
+        return evaluate(state) / value;
+      },
+      replaceNext: true
+    }
+  }
+}
+
+function evaluate(state) {
+  return state.operation != null ? state.operation(state.value) : state.value;
 }
 
 // utils
